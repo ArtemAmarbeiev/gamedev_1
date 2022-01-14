432a63b024c511eba6df054b2d82fafe05fe1c84
denezov <97740370+denezov@users.noreply.github.com>
2022-01-14 20:17:30 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,106 @@
+import pygame as pg
+from random import randint, uniform
+vec = pg.math.Vector2
+import pygame
+import math
+import Weapon
+import pygame_menu
+
+WIDTH = 800
+HEIGHT = 600
+FPS = 60
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+CYAN = (0, 255, 255)
+YELLOW = (255, 255, 0)
+DARKGRAY = (40, 40, 40)
+LIGHTGRAY = (140, 140, 140)
+
+r_val = 4
+w_val = 5
+d_val = 3
+
+walls = []
+
+# Mob properties
+MOB_SIZE = 32
+MAX_SPEED = 2
+MAX_FORCE = 0.4
+RAND_TARGET_TIME = 500
+WANDER_RING_DISTANCE = 150
+WANDER_RING_RADIUS = 50
+WANDER_TYPE = 2
+WALL_LIMIT = 30
+
+
+APPROACH_RADIUS = 120
+
+FLEE_DISTANCE = 200
+
+pg.init()
+screen = pg.display.set_mode((WIDTH, HEIGHT))
+
+all_sprites = pg.sprite.Group()
+
+class Mobby(pg.sprite.Sprite):
+    def __init__(self):
+        self.groups = all_sprites
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.image = pg.image.load("wolf.png")
+        self.image = pg.transform.scale(self.image, (20, 20))
+        self.rect = self.image.get_rect()
+        self.pos = vec(randint(0, WIDTH), randint(0, HEIGHT))
+        self.vel = vec(MAX_SPEED, 0).rotate(uniform(0, 360))
+        self.acc = vec(0, 0)
+        self.rect.center = self.pos
+
+    def follow_mouse(self):
+        mpos = pg.mouse.get_pos()
+        self.acc = (mpos - self.pos).normalize() * 0.5
+
+    def seek(self, target):
+        self.desired = (target - self.pos).normalize() * MAX_SPEED
+        steer = (self.desired - self.vel)
+        if steer.length() > MAX_FORCE:
+            steer.scale_to_length(MAX_FORCE)
+        return steer
+
+    def seek_with_approach(self, target):
+        self.desired = (target - self.pos)
+        dist = self.desired.length()
+        self.desired.normalize_ip()
+        if dist < APPROACH_RADIUS:
+            self.desired *= dist / APPROACH_RADIUS * MAX_SPEED
+        else:
+            self.desired *= MAX_SPEED
+        steer = (self.desired - self.vel)
+        if steer.length() > MAX_FORCE:
+            steer.scale_to_length(MAX_FORCE)
+        return steer
+
+    def update(self):
+        # self.follow_mouse()
+
+        for wall in walls:
+            if self.rect.colliderect(wall.rect):
+                self.kill()
+
+        if 12 == 12:
+            self.acc = self.seek_with_approach((340, 240))
+        # equations of motion
+        self.vel += self.acc
+        if self.vel.length() > MAX_SPEED:
+            self.vel.scale_to_length(MAX_SPEED)
+        self.pos += self.vel
+        if self.pos.x > WIDTH:
+            self.pos.x = 0
+        if self.pos.x < 0:
+            self.pos.x = WIDTH
+        if self.pos.y > HEIGHT:
+            self.pos.y = 0
+        if self.pos.y < 0:
+            self.pos.y = HEIGHT
+        self.rect.center = self.pos
